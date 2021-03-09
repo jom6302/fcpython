30c69efcf820081c7635af3dbe3c3a9b8ebc0642
jom6302 <79924709+jom6302@users.noreply.github.com>
2021-03-10 02:43:36 +0800

    Add files via upload

---
+++ homework(for).py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  9 23:26:23 2021
+
+@author: USER
+"""
+
+#homework1
+
+for a in range(9,0,-1):
+    for b in range(1,a+1):
+        print(b, end="")
+    print()
+    
+    
